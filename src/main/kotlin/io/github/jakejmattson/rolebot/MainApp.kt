64fd817c62229521484d424b557336c1d7c5cac0
Jake Mattson <JakeJMattson@gmail.com>
2019-02-28 10:35:52 -0600

    Create MainApp.kt

---
+++ src/main/kotlin/io/github/jakejmattson/rolebot/MainApp.kt
@@ -0,0 +1,14 @@
+package io.github.jakejmattson.rolebot
+
+import me.aberrantfox.kjdautils.api.startBot
+
+fun main(args: Array<String>) {
+    val token = args.first()
+
+    startBot(token) {
+        configure {
+            prefix = "*"
+            globalPath = "io.github.jakejmattson.rolebot"
+        }
+    }
+}
