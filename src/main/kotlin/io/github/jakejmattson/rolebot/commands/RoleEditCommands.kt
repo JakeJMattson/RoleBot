3fe1ddf281f63617f2b0df44d26cfb9a079bfdab
Jake Mattson <JakeJMattson@gmail.com>
2019-03-02 14:04:25 -0600

    Add SetName command

--- src/main/kotlin/io/github/jakejmattson/rolebot/commands/RoleEditCommands.kt
+++ src/main/kotlin/io/github/jakejmattson/rolebot/commands/RoleEditCommands.kt
@@ -5,10 +5,25 @@ import me.aberrantfox.kjdautils.api.dsl.CommandSet
 import me.aberrantfox.kjdautils.api.dsl.commands
 import me.aberrantfox.kjdautils.api.dsl.embed
 import me.aberrantfox.kjdautils.internal.command.arguments.RoleArg
+import me.aberrantfox.kjdautils.internal.command.arguments.WordArg
 import net.dv8tion.jda.core.entities.Role
 
 @CommandSet("RoleEdit")
 fun roleEditCommands() = commands {
+    command("SetName") {
+        requiresGuild = true
+        description = "Set the name of the given role."
+        expect(RoleArg, WordArg)
+        execute {
+            val role = it.args.component1() as Role
+            val newName = it.args.component2() as String
+
+            role.manager.setName(newName).queue()
+
+            it.respond("Role Name Updated")
+        }
+    }
+
     command("SetColor") {
         requiresGuild = true
         description = "Set the color of the given role."
