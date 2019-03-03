caa816294d47b47dc5961296f2fd8b248baf9bf4
Jake Mattson <JakeJMattson@gmail.com>
2019-03-03 00:39:12 -0600

    Implement BooleanArg

---
+++ src/main/kotlin/io/github/jakejmattson/rolebot/arguments/BooleanArg.kt
@@ -0,0 +1,21 @@
+package io.github.jakejmattson.rolebot.arguments
+
+import me.aberrantfox.kjdautils.api.dsl.CommandEvent
+import me.aberrantfox.kjdautils.internal.command.ArgumentResult
+import me.aberrantfox.kjdautils.internal.command.ArgumentType
+import me.aberrantfox.kjdautils.internal.command.ConsumptionType
+
+open class BooleanArg(override val name : String = "Boolean") : ArgumentType {
+    companion object : BooleanArg()
+
+    override val examples = arrayListOf("True", "true", "T")
+    override val consumptionType = ConsumptionType.Single
+    override fun convert(arg: String, args: List<String>, event: CommandEvent) =
+        when (arg.toLowerCase()) {
+            "true" -> ArgumentResult.Single(true)
+            "t" -> ArgumentResult.Single(true)
+            "false" -> ArgumentResult.Single(false)
+            "f" -> ArgumentResult.Single(false)
+            else -> ArgumentResult.Error("Invalid boolean argument.")
+        }
+    }
