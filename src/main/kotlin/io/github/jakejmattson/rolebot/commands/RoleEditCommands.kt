6805d68abbef58bc67671629f940cdda64d32313
Jake Mattson <JakeJMattson@gmail.com>
2019-03-03 00:38:01 -0600

    Improve bot responses

--- src/main/kotlin/io/github/jakejmattson/rolebot/commands/RoleEditCommands.kt
+++ src/main/kotlin/io/github/jakejmattson/rolebot/commands/RoleEditCommands.kt
@@ -1,12 +1,14 @@
 package io.github.jakejmattson.rolebot.commands
 
 import io.github.jakejmattson.rolebot.arguments.HexColorArg
+import io.github.jakejmattson.rolebot.extensions.toHexString
 import me.aberrantfox.kjdautils.api.dsl.CommandSet
 import me.aberrantfox.kjdautils.api.dsl.commands
 import me.aberrantfox.kjdautils.api.dsl.embed
 import me.aberrantfox.kjdautils.internal.command.arguments.RoleArg
 import me.aberrantfox.kjdautils.internal.command.arguments.WordArg
 import net.dv8tion.jda.core.entities.Role
+import java.awt.Color
 
 @CommandSet("RoleEdit")
 fun roleEditCommands() = commands {
@@ -17,10 +19,14 @@ fun roleEditCommands() = commands {
         execute {
             val role = it.args.component1() as Role
             val newName = it.args.component2() as String
+            val oldName = role.name
 
             role.manager.setName(newName).queue()
 
-            it.respond("Role Name Updated")
+            it.respond(embed {
+                setColor(role.color)
+                addField("Role Name Updated", "Role name changed from $oldName to $newName", false)
+            })
         }
     }
 
@@ -30,13 +36,15 @@ fun roleEditCommands() = commands {
         expect(RoleArg, HexColorArg)
         execute {
             val role = it.args.component1() as Role
-            val color = it.args.component2() as Int
+            val newColor = Color(it.args.component2() as Int)
+            val oldColor = role.color
+            val changeString = "from ${oldColor.toHexString()} to ${newColor.toHexString()}"
 
-            role.manager.setColor(color).queue()
+            role.manager.setColor(newColor).queue()
 
             it.respond(embed {
-                setColor(color)
-                title("Role Color Updated")
+                setColor(role.color)
+                addField("Role Color Updated", "Role color changed $changeString", false)
             })
         }
     }
