4bdf2df605880f6010999adb1fb406a601418a01
Jake Mattson <JakeJMattson@gmail.com>
2019-03-04 15:51:26 -0600

    Add SetMentionable command

--- src/main/kotlin/io/github/jakejmattson/rolebot/commands/RoleEditCommands.kt
+++ src/main/kotlin/io/github/jakejmattson/rolebot/commands/RoleEditCommands.kt
@@ -1,12 +1,16 @@
 package io.github.jakejmattson.rolebot.commands
 
+import io.github.jakejmattson.rolebot.arguments.BooleanArg
+import io.github.jakejmattson.rolebot.arguments.EmoteArg
 import io.github.jakejmattson.rolebot.arguments.HexColorArg
 import io.github.jakejmattson.rolebot.extensions.toHexString
+import io.github.jakejmattson.rolebot.services.GenericConvoService
 import me.aberrantfox.kjdautils.api.dsl.CommandSet
 import me.aberrantfox.kjdautils.api.dsl.commands
 import me.aberrantfox.kjdautils.api.dsl.embed
 import me.aberrantfox.kjdautils.internal.command.arguments.RoleArg
 import me.aberrantfox.kjdautils.internal.command.arguments.WordArg
+import net.dv8tion.jda.core.entities.Emote
 import net.dv8tion.jda.core.entities.Role
 import java.awt.Color
 
@@ -25,7 +29,7 @@ fun roleEditCommands() = commands {
 
             it.respond(embed {
                 setColor(role.color)
-                addField("Role Name Updated", "Role name changed from $oldName to $newName", false)
+                addField("Role Name Updated", "Role name changed from $oldName to $newName.", false)
             })
         }
     }
@@ -44,7 +48,25 @@ fun roleEditCommands() = commands {
 
             it.respond(embed {
                 setColor(role.color)
-                addField("Role Color Updated", "Role color changed $changeString", false)
+                addField("Role Color Updated", "Role color changed $changeString.", false)
+            })
+        }
+    }
+
+    command("SetMentionable") {
+        requiresGuild = true
+        description = "Set whether or not the given role can be mentioned."
+        expect(RoleArg, BooleanArg)
+        execute {
+            val role = it.args.component1() as Role
+            val mentionable = it.args.component2() as Boolean
+            val changeString = "${if (mentionable) "" else "not"} mentionable"
+
+            role.manager.setMentionable(mentionable).queue()
+
+            it.respond(embed {
+                setColor(role.color)
+                addField("Role Mentionability Updated", "Role is now $changeString.", false)
             })
         }
     }
