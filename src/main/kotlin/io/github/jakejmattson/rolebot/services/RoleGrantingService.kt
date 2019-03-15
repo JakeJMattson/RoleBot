f21eb4f0c1fc374e3a3a7759f08bc15d803fc4a2
Jake Mattson <JakeJMattson@gmail.com>
2019-03-15 00:28:33 -0500

    Add RoleGrantingService

---
+++ src/main/kotlin/io/github/jakejmattson/rolebot/services/RoleGrantingService.kt
@@ -0,0 +1,14 @@
+package io.github.jakejmattson.rolebot.services
+
+import me.aberrantfox.kjdautils.api.annotation.Service
+import net.dv8tion.jda.core.entities.Role
+
+private val grantableRoles = ArrayList<Role>()
+fun Role.isGrantable() = grantableRoles.contains(this)
+
+@Service
+class RoleGrantingService() {
+    fun addRole(role: Role) = grantableRoles.add(role)
+    fun removeRole(role: Role) = grantableRoles.remove(role)
+    fun listRoles() = grantableRoles.joinToString("\n")
+}
