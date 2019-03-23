2f36ff8d8bd5a95ec865a8e4eb9edeaf4f6278d3
Jake Mattson <JakeJMattson@gmail.com>
2019-03-22 22:53:16 -0500

    Add Uptime command

--- src/main/kotlin/io/github/jakejmattson/rolebot/commands/UtilityCommands.kt
+++ src/main/kotlin/io/github/jakejmattson/rolebot/commands/UtilityCommands.kt
@@ -2,6 +2,11 @@ package io.github.jakejmattson.rolebot.commands
 
 import me.aberrantfox.kjdautils.api.dsl.CommandSet
 import me.aberrantfox.kjdautils.api.dsl.commands
+import me.aberrantfox.kjdautils.api.dsl.embed
+import java.awt.Color
+import java.util.*
+
+private val startTime = Date()
 
 @CommandSet("utility")
 fun utilityCommands() = commands {
@@ -9,6 +14,29 @@ fun utilityCommands() = commands {
         description = "Display network status."
         execute {
             it.respond("**Pinged in**: ${it.jda.ping}ms")
+        }
+    }
+
+    command("Uptime") {
+        requiresGuild = true
+        description = "Displays how long the bot has been running."
+        execute {
+            val milliseconds = Date().time - startTime.time
+            val seconds = (milliseconds / 1000) % 60
+            val minutes = (milliseconds / (1000 * 60)) % 60
+            val hours = (milliseconds / (1000 * 60 * 60)) % 24
+            val days = (milliseconds / (1000 * 60 * 60 * 24))
+
+            it.respond(embed {
+                setColor(Color.WHITE)
+                setTitle("I have been running since")
+                setDescription(startTime.toString())
+
+                field {
+                    name = "That's been"
+                    value = "$days day(s), $hours hour(s), $minutes minute(s) and $seconds second(s)"
+                }
+            })
         }
     }
 }
